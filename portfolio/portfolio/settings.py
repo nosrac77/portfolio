74260fbe5606b6224510b7938bc61ace09d10d2c
nosrac77 <carson.newton@outlook.com>
2018-01-10 17:06:42 -0800

    modified urls.py to get rid of regex (could be a django 2.0 related issue) and added allowed_hosts var in settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-54-91-212-233.compute-1.amazonaws.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from portfolio.wesbite import views
+from website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^$', views.home_view, name='home')
+    path('', views.home_view, name='home')
 ]
