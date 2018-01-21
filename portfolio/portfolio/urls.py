8b3874a11cd2ad74e5215892a2b4e33f49dd0cb4
nosrac77 <carson.newton@outlook.com>
2018-01-20 22:16:43 -0800

    removed temp.html and replaced the url path in urls.py to render only index.html, as portfolio is now in acceptable condition.

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -19,6 +19,5 @@ from website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
-    path('portfolio/', views.portfolio_view, name='portfolio')
+    path('', views.home_view, name='home')
 ]
--- portfolio/website/views.py
+++ portfolio/website/views.py
@@ -6,10 +6,4 @@ from django.shortcuts import render
 def home_view(request):
     """Function that renders the portfolio's home view page."""
 
-    return render(request, 'website/temp.html')
-
-
-def portfolio_view(request):
-    """Function that renders the portfolio's home view page."""
-
     return render(request, 'website/index.html')
