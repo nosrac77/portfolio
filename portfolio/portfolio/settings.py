37a6b55c334c0c6fc11ab9833a5c4cafd23dc6bb
nosrac77 <carson.newton@outlook.com>
2018-01-22 23:31:09 -0800

    changed debug variable in settings.py to false in attempt to eliminate debug being set to true in deployed app

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', '')
+DEBUG = False
 
 ALLOWED_HOSTS = ['ec2-54-91-212-233.compute-1.amazonaws.com', '127.0.0.1', 'localhost', 'http://carson-newton.me/', 'carson-newton.me', 'www.carson-newton.me']
 
