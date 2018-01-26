1f1441e154987af725ee929ee491369c617fc4c0
nosrac77 <carson.newton@outlook.com>
2018-01-25 18:28:46 -0800

    created basic fade in effects for nav icons and headers

--- portfolio/website/static/website/js/app.js
+++ portfolio/website/static/website/js/app.js
@@ -2,5 +2,9 @@
 
 $(document).ready(function(){
   $('body').hide().fadeIn();
-  $('.header-fade').hide().delay(1000).fadeIn();
+  $('.header-fade').hide().delay(1000).fadeIn('slow');
+  $('.icon-user').css('opacity', '0').delay(1000).fadeTo('slow', 1.00);
+  $('.icon-file').css('opacity', '0').delay(1200).fadeTo('slow', 1.00);
+  $('.icon-briefcase').css('opacity', '0').delay(1400).fadeTo('slow', 1.00);
+  $('.icon-envelope').css('opacity', '0').delay(1600).fadeTo('slow', 1.00);
 });
