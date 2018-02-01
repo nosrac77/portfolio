5ca1dd33d0009074e44dafeb3424bcc299705275
nosrac77 <carson.newton@outlook.com>
2018-01-31 19:02:40 -0800

    made skills event listenener more modular (as opposed to creating one listener for every canvas element)

--- portfolio/website/static/website/js/app.js
+++ portfolio/website/static/website/js/app.js
@@ -7,24 +7,24 @@ $(document).ready(function(){
   $('#file-icon').css('opacity', '0').delay(1250).fadeTo('slow', 1.00);
   $('.icon-briefcase').css('opacity', '0').delay(1500).fadeTo('slow', 1.00);
   $('.icon-envelope').css('opacity', '0').delay(1750).fadeTo('slow', 1.00);
-  $('.skills-text').hide();
 });
 
-$('#python').mouseenter(function() {
+$('canvas').mouseenter(function() {
     var skillEl = $('#skills-desc');
-    var skillText = 'Python is a language invented by whats-his-face Guido something or other.';
-    if (skillEl.text() != skillText) {
-        $('.skills-text').hide().fadeIn();
-        skillEl.text(skillText);
-        $('#skills-link').attr('href', 'https://www.python.org/').text('Learn more about Python');    
-    }
-});
-$('#javascript').mouseenter(function() {
-    var skillEl = $('#skills-desc');
-    var skillText = 'JavaScript has been described as the wild-wild west of programming.';
-    if (skillEl.text() != skillText) {
-        $('.skills-text').hide().fadeIn();
-        skillEl.text(skillText);
-        $('#skills-link').attr('href', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics').text('Learn more about JavaScript');
+    var focusEl = $(this).attr('id');
+    var skillsList = [
+                 ['python', 'Python is a language invented by whats-his-face Guido something or other.', 'https://www.python.org/', 'Learn more about Python'],
+                 ['javascript', 'JavaScript has been described as the wild-wild west of programming.', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics', 'Learn more about JavaScript']
+    ]
+    for (var i = 0; i < skillsList.length; i++) {
+        if (skillsList[i][0] == focusEl) {
+            var skillText = skillsList[i][1];
+            if (skillEl.text() != skillText) {
+                $('.skills-text').hide().fadeIn();
+                skillEl.text(skillText);
+                $('#skills-link').attr('href', skillsList[i][2]).text(skillsList[i][3]);
+                break;
+            }
+        }
     }
 });
