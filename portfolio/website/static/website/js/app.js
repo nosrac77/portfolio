8fa73fca7e3b4efba63a244baa7a1099cd1e108f
nosrac77 <carson.newton@outlook.com>
2018-01-31 19:45:04 -0800

    the description for python is now satisfactory

--- portfolio/website/static/website/js/app.js
+++ portfolio/website/static/website/js/app.js
@@ -10,10 +10,12 @@ $(document).ready(function(){
 });
 
 $('canvas').mouseenter(function() {
+    $('#django-div').css('margin-left', '0px');
+    $('#htmlcss-div').css('margin-left', '0px');
     var skillEl = $('#skills-desc');
     var focusEl = $(this).attr('id');
     var skillsList = [
-                 ['python', 'Python is a language invented by whats-his-face Guido something or other.', 'https://www.python.org/', 'Learn more about Python'],
+                 ['python', 'Python is an interpreted high-level programming language known for high-readability and ease of use. Unveiled in 1991 by it\'s creator, Guido van Rossum, Python is designed with elegeance and simplicity in mind. It\'s extensive built-in support for mathematics make it well-suited for data scientists, although they\'re certainly not the only ones using it. As of January, 2018, Python sits as the fourth most popular programming language and has also been used by the likes of Yahoo, Google, NASA, CERN, and Reddit. I tend to use Python when I need to perform data analysis, quickly automate a task on my computer, or work with on a team with a large shared code-base.', 'https://www.python.org/', 'Learn more about Python'],
                  ['javascript', 'JavaScript has been described as the wild-wild west of programming.', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics', 'Learn more about JavaScript']
     ]
     for (var i = 0; i < skillsList.length; i++) {
