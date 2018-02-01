7e19c6ee69d500189b490ac9356d74244bd274c6
nosrac77 <carson.newton@outlook.com>
2018-01-31 18:05:55 -0800

    turned debug on to develop locally, modified app.js to contain jquery fade ins/outs for skill texts

--- portfolio/website/static/website/js/app.js
+++ portfolio/website/static/website/js/app.js
@@ -7,4 +7,24 @@ $(document).ready(function(){
   $('#file-icon').css('opacity', '0').delay(1250).fadeTo('slow', 1.00);
   $('.icon-briefcase').css('opacity', '0').delay(1500).fadeTo('slow', 1.00);
   $('.icon-envelope').css('opacity', '0').delay(1750).fadeTo('slow', 1.00);
+  $('.skills-text').hide();
+});
+
+$('#python').mouseenter(function() {
+    var skillEl = $('#skills-desc');
+    var skillText = 'Python is a language invented by whats-his-face Guido something or other.';
+    if (skillEl.text() != skillText) {
+        $('.skills-text').hide().fadeIn();
+        skillEl.text(skillText);
+        $('#skills-link').attr('href', 'https://www.python.org/').text('Learn more about Python');    
+    }
+});
+$('#javascript').mouseenter(function() {
+    var skillEl = $('#skills-desc');
+    var skillText = 'JavaScript has been described as the wild-wild west of programming.';
+    if (skillEl.text() != skillText) {
+        $('.skills-text').hide().fadeIn();
+        skillEl.text(skillText);
+        $('#skills-link').attr('href', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics').text('Learn more about JavaScript');
+    }
 });
