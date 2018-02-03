ec930b7c3e2ad0c44fbcc98a059ef36698e2571f
nosrac77 <carson.newton@outlook.com>
2018-02-02 17:27:20 -0800

    highlighted skills now get increased font sizes and text color changes, working on developing skill descriptions in full detail.

--- portfolio/website/static/website/js/app.js
+++ portfolio/website/static/website/js/app.js
@@ -12,17 +12,22 @@ $(document).ready(function(){
 $('canvas').mouseenter(function() {
     $('#django-div').css('margin-left', '0px');
     $('#htmlcss-div').css('margin-left', '0px');
+    $('#sony-div').css('margin-left', '0px');
+    var canvasEls = $('canvas');
     var skillEl = $('#skills-desc');
-    var focusEl = $(this).attr('id');
+    var focusEl = $(this);
     var skillsList = [
                  ['python', '<p>Python is an interpreted high-level programming language known for high-readability and ease of use. Unveiled in 1991 by it\'s creator, Guido van Rossum, Python is designed with elegeance and simplicity in mind. It\'s extensive built-in support for mathematics make it well-suited for data scientists, although they\'re certainly not the only ones using it. As of January, 2018, Python sits as the fourth most popular programming language and has also been used by the likes of Yahoo, Google, NASA, CERN, and Reddit.</p><br><p>I tend to use Python when I need to perform data analysis, quickly automate a task on my computer, or work with on a team with a large shared code-base.</p>', 'https://www.python.org/', 'Learn more about Python'],
-                 ['javascript', 'JavaScript has been described as the wild-wild west of programming.', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics', 'Learn more about JavaScript']
+                 ['javascript', '<p>Not to be confused with the popular programming language Java, JavaScript is an interpreted high-level programming language best known for providing interactive user-interfaces to web-based applications. Alongside the likes of HTML and CSS, JavaScript is a staple in developing modern websites with built-in support from all major web-browsers.</p>', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics', 'Learn more about JavaScript']
     ]
     for (var i = 0; i < skillsList.length; i++) {
-        if (skillsList[i][0] == focusEl) {
+        if (skillsList[i][0] == focusEl.attr('id')) {
             var skillText = skillsList[i][1];
-            if (skillEl.text() != skillText) {
-                alert('why');
+            if (skillEl.html() != skillText) {
+                canvasEls.next().css('color', 'white');
+                canvasEls.next().animate({fontSize: '16px'}, 200);
+                focusEl.next().animate({fontSize: '1.4em'}, 200);
+                focusEl.next().css('color', '#f85c37');
                 $('.skills-text').hide().fadeIn();
                 skillEl.html(skillText);
                 $('#skills-link').attr('href', skillsList[i][2]).text(skillsList[i][3]);
