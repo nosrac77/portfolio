b2ce312555c58d64976b73dc6c4ce50835491d59
nosrac77 <carson.newton@outlook.com>
2018-02-02 18:30:31 -0800

    completed skill description for javascript

--- portfolio/website/static/website/js/app.js
+++ portfolio/website/static/website/js/app.js
@@ -17,16 +17,16 @@ $('canvas').mouseenter(function() {
     var skillEl = $('#skills-desc');
     var focusEl = $(this);
     var skillsList = [
-                 ['python', '<p>Python is an interpreted high-level programming language known for high-readability and ease of use. Unveiled in 1991 by it\'s creator, Guido van Rossum, Python is designed with elegeance and simplicity in mind. It\'s extensive built-in support for mathematics make it well-suited for data scientists, although they\'re certainly not the only ones using it. As of January, 2018, Python sits as the fourth most popular programming language and has also been used by the likes of Yahoo, Google, NASA, CERN, and Reddit.</p><br><p>I tend to use Python when I need to perform data analysis, quickly automate a task on my computer, or work with on a team with a large shared code-base.</p>', 'https://www.python.org/', 'Learn more about Python'],
-                 ['javascript', '<p>Not to be confused with the popular programming language Java, JavaScript is an interpreted high-level programming language best known for providing interactive user-interfaces to web-based applications. Alongside the likes of HTML and CSS, JavaScript is a staple in developing modern websites with built-in support from all major web-browsers.</p>', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics', 'Learn more about JavaScript']
+                 ['python', '<p>Python is an interpreted high-level programming language known for high-readability and ease of use. Unveiled in 1991 by it\'s creator, Guido van Rossum, Python is designed with elegeance and simplicity in mind. It\'s extensive built-in support for mathematics make it well-suited for data scientists, although they\'re certainly not the only ones using it. As of January, 2018, Python sits as the fourth most popular programming language and has also been used by the likes of Yahoo, Google, NASA, CERN, and Reddit.</p><br><p>I write in Python when I need to perform data analysis, quickly automate a task on my computer, or work with on a team with a large shared code-base.</p>', 'https://www.python.org/', 'Learn more about Python'],
+                 ['javascript', '<p>Not to be confused with the popular programming language Java, JavaScript is an interpreted high-level programming language best known for providing interactive user-interfaces to web-based applications. Alongside the likes of HTML and CSS, JavaScript is a staple in developing modern websites with built-in support from all major web-browsers. It\'s implementation in webpages is so ubiquitous that it\'d actually be difficult to find a site that doesn\'t employ some level of the language. Beyond it\'s highly praised client-side capabilities, companies such as LinkedIn, Uber, Netflix, PayPal, and Ebay take advantage of JavaScript\'s server-side scripting to create fast and scalable back-end architecture.</p><br><p>I write in JavaScript when I need to create feature-rich web applications.</p>', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics', 'Learn more about JavaScript']
     ]
     for (var i = 0; i < skillsList.length; i++) {
         if (skillsList[i][0] == focusEl.attr('id')) {
             var skillText = skillsList[i][1];
             if (skillEl.html() != skillText) {
                 canvasEls.next().css('color', 'white');
-                canvasEls.next().animate({fontSize: '16px'}, 200);
-                focusEl.next().animate({fontSize: '1.4em'}, 200);
+                canvasEls.next().animate({fontSize: '16px'}, 100);
+                focusEl.next().animate({fontSize: '1.4em'}, 180);
                 focusEl.next().css('color', '#f85c37');
                 $('.skills-text').hide().fadeIn();
                 skillEl.html(skillText);
